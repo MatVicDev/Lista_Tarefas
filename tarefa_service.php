9a586198c27a3fa2160c5bbc84cc63321ff86556
MatVicDev <matheus@teste.com>
2021-09-23 10:04:29 -0300

    Criando a classe tarefa service

---
+++ tarefa_service.php
@@ -0,0 +1,28 @@
+<?php
+
+class TarefaService
+{
+	// CRUD
+	public function inserir() // Create
+	{
+
+	}
+
+	public function recuperar() // Read
+	{
+
+	}
+
+	public function atualizar() // Update
+	{
+
+	}
+
+	public function remover() // Delete
+	{
+
+	}
+}
+
+
+?>
